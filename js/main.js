8d0c5c508d3eb87bec4a1aff9127aa2e8db92469
Liu <y_liu85714@tss-hr420-dm12.fc.ca>
2019-01-14 16:26:20 -0500

    finish the javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(() => {
+	// self-invoking anonymous function (or lambda)
+	// 
+	// var =>means creat a variable
+	// whatever comes after the = is what the variable ref
+	var images = document.querySelectorAll('img');
+
+	//document is always the webpage
+	//after the dot is always a method or a property
+	
+	console.log(images);
+
+})();
